397dcea1de5129fc18bdaa6c6395a3af7f9de404
Morgana Galamba <mbfg2@cin.ufpe.br>
2021-07-06 17:43:10 -0300

    create project

---
+++ ItsLit/ItsLit/ItsLitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsLitApp.swift
+//  ItsLit
+//
+//  Created by Morgana Galamba on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ItsLitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
