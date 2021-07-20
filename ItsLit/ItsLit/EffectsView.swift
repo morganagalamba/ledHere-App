07a288a27cd7ea4609c887b04068060e51d42749
Morgana Galamba <mbfg2@cin.ufpe.br>
2021-07-20 17:53:14 -0300

    add effects view

---
+++ ItsLit/ItsLit/EffectsView.swift
@@ -0,0 +1,20 @@
+//
+//  EffectsView.swift
+//  ItsLit
+//
+//  Created by Morgana Galamba on 20/07/21.
+//
+
+import SwiftUI
+
+struct EffectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EffectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        EffectsView()
+    }
+}
