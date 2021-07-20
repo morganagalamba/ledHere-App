e6b6ac380ced77df0bf57092d90f28b48fcb4e71
eduardolopesc <eduardo.lopes.c@gmail.com>
2021-07-20 16:58:36 -0300

    Fix r g b

--- ItsLit/ItsLit/ContentView.swift
+++ ItsLit/ItsLit/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //ColourWheelView(viewModel: ColourWheelViewModel())
-        DragView()
+        ColourWheelView(viewModel: ColourWheelViewModel())
+//        DragView()
     }
 }
 
