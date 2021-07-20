7789b6ff3efc69a40c9790f29ca446d4c3df34b1
Morgana Galamba <mbfg2@cin.ufpe.br>
2021-07-20 16:33:45 -0300

    add projectView

--- ItsLit/ItsLit/ContentView.swift
+++ ItsLit/ItsLit/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         //ColourWheelView(viewModel: ColourWheelViewModel())
-        DragView()
+        //DragView()
+        ProjectView()
     }
 }
 
--- ItsLit/ItsLit/ProjectView.swift
+++ ItsLit/ItsLit/ProjectView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ProjectView: View {
     var body: some View {
         HStack{
-            Spacer()
-                
+            SimulationView()
+            ControlColorView()
         }
     }
 }
@@ -19,5 +19,32 @@ struct ProjectView: View {
 struct ProjectView_Previews: PreviewProvider {
     static var previews: some View {
         ProjectView()
+            .landscape()
+    }
+}
+
+struct LandscapeModifier: ViewModifier {
+    let height = UIScreen.main.bounds.width
+    let width = UIScreen.main.bounds.height
+    
+    var isPad: Bool {
+        return height >= 768
+    }
+    
+    var isRegularWidth: Bool {
+        return height >= 414
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .previewLayout(.fixed(width: width, height: height))
+            .environment(\.horizontalSizeClass, isRegularWidth ? .regular: .compact)
+            .environment(\.verticalSizeClass, isPad ? .regular: .compact)
+    }
+}
+
+extension View {
+    func landscape() -> some View {
+        self.modifier(LandscapeModifier())
     }
 }
